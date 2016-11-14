8a5a6f0d0f89ad6f1d2423c0081c7739bf09344b
Andy Gillon <aawgillon@gmail.com>
2016-11-14 19:04:26 +0000

    forms module

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -9,10 +9,23 @@ export class ShoppingListService {
   	return this.items;
   }
 
+  addItem(item: Ingredient) {
+    this.items.push(item);
+  }
+
   addItems(items: Ingredient[]) {
   	//Array.prototype.push.apply(this.items, items);
   	this.items.push(...items);
   	// love the spread operator
   }
+
+  editItem(oldItem: Ingredient, newItem: Ingredient) {
+    this.items[this.items.indexOf(oldItem)] = newItem;
+  }
+
+  deleteItem(item: Ingredient) {
+    this.items.splice(this.items.indexOf(item), 1);
+  }
+  
 
 }
